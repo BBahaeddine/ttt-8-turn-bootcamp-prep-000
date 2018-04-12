a37bcaee57004249193fc44a158460e8898d8336
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-04-12 14:05:22 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,14 @@ def display_board(board)
   puts "-----------"
   puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
+def valid_move?(board, index)
+  if index.between?(0, 8)
+    if position_taken?(board, index)
+      return false
+    else
+      return true
+    end
+  else
+    return nil
+  end
+end
